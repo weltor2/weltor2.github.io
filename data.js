f357fc9208336e768492039e1a6f8f907163e0d7
weltor2 <wiktorszurik@wp.pl>
2024-03-19 20:38:17 +0100

    s

--- data.js
+++ data.js
@@ -1,34 +1,42 @@
 data = new Date();
+function ciasteczko(name,status){
+    d = new Date()
+    d.setTime(data.getTime() + (32 *24*60*60*1000))
+    let expr = "expires="+d.toUTCString()
+    document.cookie = name + '=' + status + ';' + expr + ";path=/cookies"
+
+}
+
+
+
 
 for (let i = 0; i < document.querySelectorAll('.day').length; i++) {
     data.setDate((new Date).getDate()-1+i)
     document.querySelectorAll('.day')[i].innerHTML =`${data.getDate()}.${data.getMonth()+1}`    
 }
-// obowiazki = document.querySelectorAll('.chores')
-// obowiazkiT= Array.from(obowiazki) 
-// for (let i = 0; i < obowiazki.length; i++) {   
-//     obowiazki[i].addEventListener('click',event=>{
-//         event.preventDefault()
-//         index = obowiazkiT.indexOf(event.target)
-//         console.log(index)
-//         if(obowiazki[index].innerHTML != ``){
-//             console.log('bee')
-//         }
-//     })
 
-// }
 
 for (let i = 0; i < document.querySelectorAll('#waterTBD').length; i++) {
     document.querySelectorAll('#waterTBD')[i].addEventListener('click',event=>{
         event.preventDefault()
         if(event.target.src.slice(-11) =='notDone.png' ){
             event.target.src = './done.png'
+            //ciasteczko('19.03',true)
+            
 
         }
         else{
             event.target.src = './notDone.png'
+            //ciasteczko('19.03',false)
+
         }
         
     })
     
 }
+document.cookie = "username=John Doe"
+
+function showCookies() {
+    const output = document.getElementById("cookies");
+    output.textContent = `> ${document.cookie}`;
+  }
