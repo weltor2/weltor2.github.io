7b5c6cca9830d1479c6b0deeae22328dafec87c8
weltor2 <wiktorszurik@wp.pl>
2024-03-19 14:27:44 +0100

    update

--- data.js
+++ data.js
@@ -1,7 +1,6 @@
 data = new Date();
-console.log(data.getMonth()+1,data.getDate())
 
 for (let i = 0; i < document.querySelectorAll('.day').length; i++) {
-    document.querySelectorAll('.day')[i].innerHTML =`${new Date().getDate()-1+i}.${new Date().getMonth()+1}`    
+    data.setDate((new Date).getDate()-1+i)
+    document.querySelectorAll('.day')[i].innerHTML =`${data.getDate()}.${data.getMonth()+1}`    
 }
-//https://stackoverflow.com/questions/23081158/javascript-get-date-of-the-next-day
