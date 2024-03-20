cc21c2f94bc21d6afc03e5d13894b4bd74817606
weltor2 <wiktorszurik@wp.pl>
2024-03-20 19:22:03 +0100

    ALL

--- data.js
+++ data.js
@@ -8,6 +8,8 @@ cheks = document.querySelectorAll('#waterTBD')
 cheksT = Array.from(cheks)
 days= document.querySelectorAll('.day')
 
+spray = document.querySelectorAll('#sprayTBD')
+sprayT = Array.from(spray)
 const nazwa = (document.querySelector('#srodek').innerText).split(' ')[0] 
 
 
@@ -30,13 +32,30 @@ function getCookie(cname) {
 
 ///////////////////////All Plants//////////////////////////
 
-
-Peperomia= {daysTW:2 , firstW:'Mon Mar 20 2024'}  ///date.todatestring()
-
-
+Asplenium = {daysTW:4 , daysTS:2, firstW:'Mon Mar 21 2024'}
+Beaucarnea ={daysTW:14 , daysTS:0, firstW:'Mon Mar 21 2024'}
+Begonia = {daysTW:6 , daysTS:0, firstW:'Mon Mar 21 2024'}
+Biophytum ={daysTW:6 , daysTS:0, firstW:'Mon Mar 21 2024'}
+Calathea = {daysTW:7 , daysTS:1, firstW:'Mon Mar 21 2024'}
+Ceropegia = {daysTW:10 , daysTS:0, firstW:'Mon Mar 21 2024'}
+Clematicissus = {daysTW:5 , daysTS:1, firstW:'Mon Mar 21 2024'}
+Coffea = {daysTW:5 , daysTS:1, firstW:'Mon Mar 21 2024'}
+Dischidia = {daysTW:4 , daysTS:4, firstW:'Mon Mar 21 2024'}
+Dracaena ={daysTW:14 , daysTS:3, firstW:'Mon Mar 21 2024'}
+Epiphyllum = {daysTW:7 , daysTS:4, firstW:'Mon Mar 21 2024'}
+Monkey = {daysTW:4 , daysTS:1, firstW:'Mon Mar 21 2024'}
+Nematanthus = {daysTW:7 , daysTS:3, firstW:'Mon Mar 21 2024'}
+Nephrolepis ={daysTW:5 , daysTS:1, firstW:'Mon Mar 21 2024'}
+Peperomia = {daysTW:7 , daysTS:0, firstW:'Mon Mar 21 2024'}  ///date.todatestring()
+Philodendron = {daysTW:7 , daysTS:2, firstW:'Mon Mar 21 2024'}
+Pilea = {daysTW:7 , daysTS:1, firstW:'Mon Mar 21 2024'}
+Sansewieria = {daysTW:21 , daysTS:0, firstW:'Mon Mar 21 2024'}
 
 /////////////////////////////////////////////////////////////////
 
+if(eval(nazwa).daysTS == 0){
+    document.querySelector('#kalendarz').querySelectorAll('tr')[2].style.display = 'none'
+}
 
 for (let i = 0; i < days.length; i++) {
     data.setDate((new Date).getDate()-1+i)
@@ -46,9 +65,15 @@ for (let i = 0; i < days.length; i++) {
     
     if(daysApart%eval(nazwa).daysTW == 0){
         cheks[i].style.display = 'inline'
-        document.cookie = `${nazwa}/${daysT[i]} = false`
+        document.cookie = `${nazwa}/water/${daysT[i]} = false`
+    }
+    if(daysApart%eval(nazwa).daysTS == 0){
+        spray[i].style.display = 'inline'
+        document.cookie = `${nazwa}/spray/${daysT[i]} = false`
     }
 }
+
+
 
 for (let i = 0; i < days.length; i++) {
     daysT.push(days[i].innerHTML)
@@ -60,39 +85,53 @@ for (let i = 0; i < days.length; i++) {
 for (let i = 0; i < cheks.length; i++) {
     cheks[i].addEventListener('click',event=>{
         event.preventDefault()
-
         clickI =(cheksT.indexOf(event.target))
-
-
         if(event.target.src.slice(-11) =='notDone.png' ){
             event.target.src = './sideImg/done.png'
-            document.cookie = `${nazwa}/${daysT[clickI]} = true`
-            
-
+            document.cookie = `${nazwa}/water/${daysT[clickI]} = true`
         }
         else{
             event.target.src = './sideImg/notDone.png'
-            document.cookie = `${nazwa}/${daysT[clickI]} = false`
+            document.cookie = `${nazwa}/water/${daysT[clickI]} = false`
 
+        }  
+    })   
+}
+
+for (let i = 0; i < spray.length; i++) {
+    spray[i].addEventListener('click',event=>{
+        event.preventDefault()
+        clickI =(sprayT.indexOf(event.target))
+        if(event.target.src.slice(-11) =='notDone.png' ){
+            event.target.src = './sideImg/done.png'
+            document.cookie = `${nazwa}/spray/${daysT[clickI]} = true`
         }
-        
-        
-    })
-    
+        else{
+            event.target.src = './sideImg/notDone.png'
+            document.cookie = `${nazwa}/spray/${daysT[clickI]} = false`
+        }  
+    })   
 }
 
 
 
-
 for (let i = 0; i < daysT.length; i++) {
-    if(getCookie(`${nazwa}/${daysT[i]}`) == 'true'){
+    if(getCookie(`${nazwa}/water/${daysT[i]}`) == 'true'){
         cheks[i].style.display = 'inline'
         cheks[i].src = './sideImg/done.png'
     } 
-    else if(getCookie(`${nazwa}/${daysT[i]}`) == 'false'){
+    else if(getCookie(`${nazwa}/water/${daysT[i]}`) == 'false'){
         cheks[i].style.display = 'inline'
         cheks[i].src = './sideImg/notDone.png'
-    }   
+    }
+    if(getCookie(`${nazwa}/spray/${daysT[i]}`) == 'true'){
+        cheks[i].style.display = 'inline'
+        cheks[i].src = './sideImg/done.png'
+    } 
+    else if(getCookie(`${nazwa}spray/${daysT[i]}`) == 'false'){
+        cheks[i].style.display = 'inline'
+        cheks[i].src = './sideImg/notDone.png'
+    }
 }
 
 
